90b37da781daeebe99aaa834fabf5459aee1a843
NightPotato <drickman180@gmail.com>
2025-02-16 14:20:11 -0500

    upload current progress

---
+++ Core.cs
@@ -0,0 +1,29 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(ExtraTerror.Core), "ExtraTerror", "1.0.0", "NightPotato", null)]
+[assembly: MelonGame("TeamMonumental", "Subterror")]
+namespace ExtraTerror;
+
+public class Core : MelonMod
+{
+    private MelonPreferences_Category extraTerrorCategory;
+    public MelonPreferences_Entry<int> maxPlayerCount;
+
+    public override void OnInitializeMelon()
+    {
+        // Mod Configuration File
+        extraTerrorCategory = MelonPreferences.CreateCategory("ExtraTerror");
+        extraTerrorCategory.SetFilePath("UserData/ExtraTerror.cfg");
+        maxPlayerCount = extraTerrorCategory.CreateEntry<int>("MaxPlayerCount", 8);
+        extraTerrorCategory.SaveToFile();
+
+        // Get Count of Loaded Players
+        var foundSouls = GameObject.FindObjectsOfType<TeamMonumental.Soul>();
+        int soulsCount = foundSouls.Length;
+
+        // Logging to See our Mod has Started
+        LoggerInstance.Msg("Initialized.");
+    }
+
+}
---
+++ GameNetworkManagerPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using TeamMonumental;
+using System.Reflection;
+using HarmonyLib.Tools;
+using MelonLoader;
+
+namespace ExtraTerror
+{
+    [HarmonyPatch(typeof(GameNetworkManager), "Awake")]
+    class GameNetworkManagerPatch
+    {
+        private static void Postfix()
+        {
+
+            MelonLogger.Msg("Starting Patch of GameNetworkManager....");
+
+            Type gameManager = typeof(GameNetworkManager);
+            FieldInfo playerCount = gameManager.GetField("MAX_PLAYER_COUNT");
+            playerCount.SetValue(playerCount, MelonPreferences.GetEntryValue<int>("ExtraTerror", "MaxPlayerCount"));
+
+            MelonLogger.Msg("Successfully Patched GameNetworkManager!!!");
+        }
+
+    }
+}
